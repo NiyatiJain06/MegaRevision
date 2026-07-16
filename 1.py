84e9304127083cb6458811228df7de07682c02a7
Niyati Jain <Jainniyati905@gmail.com>
2026-07-16 23:19:43 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+#Takes a number to a user and print their : sum, difference, product.
+
+Number1 = float(input("Enter the  first number:"))
+Number2 = float(input("Enter the second number:"))
+print("fsum:", Number1+Number2)
+print("fdifference:", Number1-Number2)
+print("fproduct:", Number2*Number2)
