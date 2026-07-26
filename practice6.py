447dea10a62c14dd8688de6c162a841d683185ad
Niyati Jain <Jainniyati905@gmail.com>
2026-07-26 21:50:23 +0530

    Add files via upload

---
+++ practice6.py
@@ -0,0 +1,19 @@
+# list of my skills.
+
+My_skills = ["Python", "DSA", "BlockChain", "Java", "HTML"]
+print(len(My_skills))
+print("Enter the My skills:",My_skills[1])
+
+My_skills.append("AI/ML")
+print(My_skills)
+My_skills.pop(1)
+print(My_skills)
+My_skills.remove("HTML")
+print(My_skills)
+My_skills.insert(1,"HTML")
+print(My_skills)
+My_skills[0]="Python Pro"
+print(My_skills)
+for skill in My_skills:
+    print(My_skills)
+
---
+++ practice7.py
@@ -0,0 +1,14 @@
+# take 3 food to a user  and store in list, print list and length.
+
+food1 = input("Enter food 1:")
+food2 = input("Enter food 2:")
+food3 = input("Enter food 3:")
+
+foodsList = []
+foodsList.append(food1)
+foodsList.append(food2)
+foodsList.append(food3)
+
+print(foodsList)
+print(len(foodsList))
+
