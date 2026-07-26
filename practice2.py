0ec8a7c6f05bafa14ff15846afba959b93783c14
Niyati Jain <Jainniyati905@gmail.com>
2026-07-26 21:47:44 +0530

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,19 @@
+# write a program to take two number to a user 
+# do addition, subtraction, multipication, division
+# and convert value in float
+
+num1 = int(input("Enter the value:"))
+num2 = int(input("Enter the value:"))
+sum = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print(sum)
+print(subtraction)
+print(multiplication)
+print(division)
+convertedValue1 = float(num1)
+convertedValue2 = float(num2)
+print("Converted Value of num1 is:", convertedValue1)
+print("Converted Value of num2 is:", convertedValue2)
+
