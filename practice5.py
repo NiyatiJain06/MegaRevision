1682d114f6fd72428d1203e5b388b20a0a69a677
Niyati Jain <Jainniyati905@gmail.com>
2026-07-26 21:49:47 +0530

    Add files via upload

---
+++ practice5.py
@@ -0,0 +1,12 @@
+#calling a function hello print Hello Niyati, yor are doing great!
+
+def helloFunc ():
+    print("Hello Niyati, You are doing great!")
+
+helloFunc()
+
+helloFunc()
+
+helloFunc()
+
+helloFunc()
