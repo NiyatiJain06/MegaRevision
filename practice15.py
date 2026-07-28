065125d8e6678d88a29343e6cf26ae8dc063fc08
Niyati Jain <Jainniyati905@gmail.com>
2026-07-28 22:13:12 +0530

    Add files via upload

---
+++ practice15.py
@@ -0,0 +1,15 @@
+class Vehicle:
+    color = "Black"
+    PetrolOrDiesel = "Pertrol"
+    mileage = "10"
+
+car = Vehicle()
+print(car.color)
+
+bike = Vehicle()
+print(bike.PetrolOrDiesel)
+
+aereoplane = Vehicle()
+print(aereoplane.mileage)
+print(aereoplane.color)
+
