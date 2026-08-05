f1bfadd7491f00997d671868761e709678446f8a
Niyati Jain <Jainniyati905@gmail.com>
2026-08-05 23:29:35 +0530

    Add files via upload

---
+++ explicit.py
@@ -0,0 +1,8 @@
+# take num as input
+# convert to float and print both orginal & converted
+# value with their data types
+
+num = input("Enter a value:")
+convertedValue = float(num)
+print("Orginal value is", num, "Data type:",type(num))
+print("Converted value is", convertedValue, "Data type:",type(convertedValue))
