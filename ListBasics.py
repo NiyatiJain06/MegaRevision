1cb25d2f61cffe6511c087d28f8dff23fc9caf55
Niyati Jain <Jainniyati905@gmail.com>
2026-08-07 22:54:50 +0530

    Add files via upload

---
+++ ListBasics.py
@@ -0,0 +1,12 @@
+# Lists in Python
+
+food = ["Chole bathure", "ice cream", "dosa", "mango", "capacino", "kaju katli"]
+print(food[0])
+print(len(food))
+
+# list are mutable
+food[2] = "pizza"
+print(food)
+
+#Slicing
+print(food[1:4])
---
+++ stringBasics.py
@@ -0,0 +1,24 @@
+str1 = ' Hello '
+str2 = "Niyati Jain"
+str3 = '''Welcome to Python!'''
+
+print(str1)
+print(str2)
+print(str3)
+
+# string concatenation
+print(str1 + " " + str2)
+
+#length of string
+print(len(str1))
+
+# indexing 
+print(str1[2])
+print(str1[0:3])
+print(str1)
+
+# Negative indexing
+print(str1[-5:-1])
+print(str1.split(","))
+print("-".join(str1))
+print(str1.strip())
